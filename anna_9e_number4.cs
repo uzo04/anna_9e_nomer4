b373ea654df4a984150ecefc3c6a50a3b24847d6
uzo04 <119493700+uzo04@users.noreply.github.com>
2023-01-04 12:18:24 +0200

    Add files via upload

---
+++ anna_9e_number4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+           int[] arr = new int[n];
+           int min;
+
+           for(int i = 0; i < n; i++)
+           {
+            arr[i] = int.Parse(Console.ReadLine());
+           }
+           min = arr[0]
+           for (int i = 0; i < n; i++)
+           {
+            if (arr[i] < min) min = arr[i];
+           }
+        Console.WriteLine(min)
+        }
+    }
+}
